89275d3e0be0087e677fbf9b324db8f690e46e17
NessMC <contact@nessmc.fr>
2020-06-01 15:26:21 +0200

    🔨 Channels template generation added.

--- commands/add.js
+++ commands/add.js
@@ -9,15 +9,51 @@ export default {
 
         constructor (client, message, args) {
 
-            this.client  = client
-            this.message = message
-            this.args    = args
+            this.client   = client
+            this.message  = message
+            this.args     = args
+            this.template = {
+                channels : [],
+                roles    : []
+            }
 
         }
 
         command () {
 
-            this.message.channel.send('add')
+            for (const channel of this.message.guild.channels) {
+                const informations = channel[1],
+                      id           = (([...informations.name].map(x => x.charCodeAt()).reduce((acc, cur) => acc + cur) << 19).toString(36))
+                if (informations.type === 'category') {
+                    this.template.channels.push({
+                        name        : informations.name,
+                        type        : informations.type,
+                        id          : id,
+                        channelID   : informations.id,
+                        permissions : informations.permissionOverwrites,
+                        children    : []
+                    })
+                } else {
+                    if (informations.parentID) {
+                        this.template.channels[this.template.channels.indexOf(this.template.channels.filter(x => x.channelID === informations.parentID)[0])].children.push({
+                            name        : informations.name,
+                            type        : informations.type,
+                            id          : id,
+                            channelID   : informations.id,
+                            permissions : informations.permissionOverwrites
+                        })
+                    } else {
+                        this.template.channels.push({
+                            name        : informations.name,
+                            type        : informations.type,
+                            id          : id,
+                            channelID   : informations.id,
+                            permissions : informations.permissionOverwrites
+                        })
+                    }
+                }
+            }
+            // console.log(this.template)
 
         }
 
