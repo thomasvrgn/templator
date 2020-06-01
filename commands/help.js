401b377a692a7d3bb3e7f8018fc1f81ed242fbc8
NessMC <contact@nessmc.fr>
2020-06-01 14:53:41 +0200

    🔨 Blank fields removed

--- commands/help.js
+++ commands/help.js
@@ -17,9 +17,9 @@ export default {
 
         command () {
             const embed = new Discord.RichEmbed().setTitle('Page d\'aide')
-                                                    .setDescription('Affiche la page d\'aide du bot')
-                                                    .setFooter('Demandé par ' + this.message.author)
-                                                    .setAuthor(this.client.user.username + '#' + this.client.user.discriminator)
+                                                 .setDescription('Affiche la page d\'aide du bot')
+                                                 .setFooter('Demandé par ' + this.message.author)
+                                                 .setAuthor(this.client.user.username + '#' + this.client.user.discriminator)
             for (const command of this.client.commands) {
                 const COMMAND = command[1]
                 let   name    = COMMAND.name,
@@ -33,7 +33,6 @@ export default {
                 if (!cmd)  cmd  = 'Aucune commande.'
                 
                 embed.addField(`t!${name}`, desc + '\n```' + cmd + '```', true)
-                embed.addBlankField(true)
             }
 
             this.message.channel.send(embed)
