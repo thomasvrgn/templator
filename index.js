60e180434aedb5ab5e14cf51661871935a27c2ff
NessMC <contact@nessmc.fr>
2020-06-02 11:28:25 +0200

    💄 New loading UI added.

--- index.js
+++ index.js
@@ -14,61 +14,92 @@ const CONFIG          = ENV.config().parsed,
       client          = new Discord.Client()
       client.commands = new Discord.Collection()
 
-FS.exists(PATH.resolve(PATH.join(__dirname, 'commands')), stats => {
-    
-    if (!stats) throw new Error ('Command folder does not exist!')
+class TemplatorLoader {
 
-    FS.readdir(PATH.resolve(PATH.join(__dirname, 'commands')), (error, content) => {
-        if (error) throw error
-        if (content.length < 1) throw new Error ('There are no commands in the folder!')
+    constructor () {}
 
-        for (const file in content) {
+    title (callback) {
+        console.log(BOXEN(CHALK.grey(FIGLET.textSync('TEMPLATOR', {font: 'Rectangles'})), {padding: 1}))
+        callback()
+    }
 
-            import(PATH.resolve(PATH.join(__dirname, 'commands', content[file]))).then(values => {
-                const COMMAND   = values.default,
-                      EXEC      = new COMMAND.run()
+    load () {
 
-                let   name      = COMMAND.name,
-                      desc      = COMMAND.description,
-                      category  = COMMAND.category
 
-                if (!name) name = content[file].split('.')[0]
-                if (!desc) desc = 'Some description'  
-                if (!EXEC) return console.log(CHALK.bgRed.bold(' ERROR '), 'Command', CHALK.grey.bold(name), 'does not have runnable code.')
-                console.log(CHALK.bgBlue.bold(' LOADED '), 'Command', CHALK.grey.bold(name), 'has been loaded.')
+        this.title(() => {
+            FS.exists(PATH.resolve(PATH.join(__dirname, 'commands')), stats => {
 
-                // client.commands.set(name, COMMAND, category)
+                if (!stats) return console.log('  ' + CHALK.bgRed.bold(' ERROR '), 'The command folder does not exists.')
+            
+                FS.readdir(PATH.resolve(PATH.join(__dirname, 'commands')), (error, content) => {
+                    if (error) throw error
+                    if (content.length < 1) return console.log('  ' + CHALK.bgRed.bold(' ERROR '), 'No commandes were found in the folder.')
+
+                    console.log('\n • COMMANDS:\n')
+            
+                    for (const file in content) {
+            
+                        import(PATH.resolve(PATH.join(__dirname, 'commands', content[file]))).then(values => {
+                            const COMMAND   = values.default,
+                                    EXEC      = new COMMAND.run()
+            
+                            let   name      = COMMAND.name,
+                                    desc      = COMMAND.description,
+                                    category  = COMMAND.category
+            
+                            if (!name) name = content[file].split('.')[0]
+                            if (!desc) desc = 'Some description'  
+                            if (!EXEC) return console.log('  ' + CHALK.bgRed.bold(' ERROR '), 'Command', CHALK.grey.bold(name), 'does not have runnable code.')
+                            console.log('  ' + CHALK.bgBlue.bold(' LOADED '), 'Command', CHALK.grey.bold(name), 'has been loaded.')
+            
+                            // client.commands.set(name, COMMAND, category)
+                        }).then(() => {
+                            if (parseInt(file) + 1 === content.length) {
+                                FS.exists(PATH.resolve(PATH.join(__dirname, 'events')), stats => {
+                
+                                    if (!stats) return console.log('  ' + CHALK.bgRed.bold(' ERROR '), 'The events folder does not exists.')
+                                
+                                    FS.readdir(PATH.resolve(PATH.join(__dirname, 'events')), (error, content) => {
+                                        if (error) throw error
+                                        if (content.length < 1) return console.log('  ' + CHALK.bgRed.bold(' ERROR '), 'No events were found in the folder.')
+
+                                        console.log('\n • EVENTS:\n')
+                                
+                                        for (const file in content) {
+                                
+                                            import(PATH.resolve(PATH.join(__dirname, 'events', content[file]))).then(values => {
+                                                const EVENT     = values.default,
+                                                        EXEC      = new EVENT.run
+                                                let   name      = EVENT.name
+                                
+                                                if (!name) name = content[file].split('.')[0]
+                                                if (!EVENT.run) return console.log('  ' + CHALK.bgRed.bold(' ERROR '), 'Event', CHALK.grey.bold(name), 'does not have runnable code.')
+                                                console.log('  ' + CHALK.bgGreen.grey.bold(' LOADED '), 'Event', CHALK.grey.bold(name), 'has been loaded.')
+                                                // client.on(name, EXEC.event.bind(null, client))
+                                            }).then(() => {
+                                                if (parseInt(file) + 1 === content.length) {
+                                                    console.log('\n  ' + CHALK.bgCyan.grey(' SUCCESS '), 'Commands and events were loaded.')
+                                                }
+                                            })
+                                
+                                        }
+                                    })
+                                
+                                })
+                            }
+                        })
+            
+                    }
+                })
+            
             })
+        })
+        
+        
+    }
 
-        }
-    })
+}
 
-})
-
-FS.exists(PATH.resolve(PATH.join(__dirname, 'events')), stats => {
-    
-    if (!stats) throw new Error ('Command folder does not exist!')
-
-    FS.readdir(PATH.resolve(PATH.join(__dirname, 'events')), (error, content) => {
-        if (error) throw error
-        if (content.length < 1) throw new Error ('There are no commands in the folder!')
-
-        for (const file in content) {
-
-            import(PATH.resolve(PATH.join(__dirname, 'events', content[file]))).then(values => {
-                const EVENT     = values.default,
-                      EXEC      = new EVENT.run
-                let   name      = EVENT.name
-
-                if (!name) name = content[file].split('.')[0]
-                if (!EVENT.run) return onsole.log(CHALK.bgRed.bold(' ERROR '), 'Event', CHALK.grey.bold(name), 'does not have runnable code.')
-                console.log(CHALK.bgGreen.grey.bold(' LOADED '), 'Event', CHALK.grey.bold(name), 'has been loaded.')
-                // client.on(name, EXEC.event.bind(null, client))
-            })
-
-        }
-    })
-
-})
+new TemplatorLoader().load()
 
 client.login(CONFIG.TOKEN)
