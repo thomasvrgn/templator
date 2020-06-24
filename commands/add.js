434d9fb74d060038ea987bfae96a6c8e8fd96dbc
NessMC <contact@nessmc.fr>
2020-06-24 10:18:39 +0200

    🧼 Code cleaned up.

--- commands/add.js
+++ commands/add.js
@@ -25,9 +25,12 @@ export default {
         command () {
 
             for (const channel of this.message.guild.channels) {
+
                 const informations = channel[1],
                       id           = (([...informations.name].map(x => x.charCodeAt()).reduce((acc, cur) => acc + cur) << 19).toString(36))
+                
                 if (informations.type === 'category') {
+
                     this.template.channels.push({
                         name        : informations.name,
                         type        : informations.type,
@@ -36,8 +39,11 @@ export default {
                         permissions : informations.permissionOverwrites,
                         children    : []
                     })
+
                 } else {
+
                     if (informations.parentID) {
+
                         this.template.channels[this.template.channels.indexOf(this.template.channels.filter(x => x.channelID === informations.parentID)[0])].children.push({
                             name        : informations.name,
                             type        : informations.type,
@@ -45,7 +51,9 @@ export default {
                             channelID   : informations.id,
                             permissions : informations.permissionOverwrites
                         })
+
                     } else {
+
                         this.template.channels.push({
                             name        : informations.name,
                             type        : informations.type,
@@ -53,11 +61,17 @@ export default {
                             channelID   : informations.id,
                             permissions : informations.permissionOverwrites
                         })
+
                     }
+
                 }
+
             }
+
             for (const role of this.message.guild.roles) {
+
                 const informations = role[1]
+
                 this.template.roles.push({
                     name        : informations.name,
                     id          : informations.id,
@@ -66,13 +80,19 @@ export default {
                     permissions : informations.permissions,
                     mentionable : informations.mentionable
                 })
+
             }
+
             FS.exists(PATH.resolve('templates'), bool => {
+                
                 if (!bool) return
+
                 FS.writeFile(PATH.resolve(PATH.join('templates', (([...this.args[0]].map(x => x.charCodeAt()).reduce((acc, cur) => acc + cur) << 19).toString(36)) + '.js')), 'module.exports=' + JSON.stringify(this.template) + '', error => {
                     if (error) throw error
                 })
+
             })
+
 
         }
 
